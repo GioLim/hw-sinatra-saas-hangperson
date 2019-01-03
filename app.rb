afb12dab4101d987a5e03a693d935af707e135fb
Ubuntu <ubuntu@ip-172-31-27-63.us-east-2.compute.internal>
2019-01-03 17:48:22 +0000

    pass -all tests-

--- app.rb
+++ app.rb
@@ -39,7 +39,6 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
     begin
       if @game.guess(letter) == false
         flash[:message] = "You have already used that letter."
@@ -57,7 +56,14 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    result = @game.check_win_or_lose
+    if result == :win
+      redirect '/win'
+    elsif result == :lose
+      redirect '/lose'
+    else
+      erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
