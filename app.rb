d9d29f1e4ec29a6d27abac31af6df84050ee7318
Ubuntu <ubuntu@ip-172-31-27-63.us-east-2.compute.internal>
2019-01-03 17:31:22 +0000

    pass -repeated_guess- test

--- app.rb
+++ app.rb
@@ -40,7 +40,13 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    @game.guess(letter)
+    begin
+      if @game.guess(letter) == false
+        flash[:message] = "You have already used that letter."
+      end
+    rescue
+      flash[:message] = "Invalid guess."
+    end
     redirect '/show'
   end
   
